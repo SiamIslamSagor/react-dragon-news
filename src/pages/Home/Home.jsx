f5628aacfd3e8241abb1b37daed2ac1485e11033
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-03 22:01:29 +0600

    added tailwind coustom font and create tow component for layOut

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
+import Header from "../Sheard/Header/Header";
+import Navbar from "../Sheard/Navbar/Navbar";
+
 const Home = () => {
   return (
     <div>
-      <h2>This is home</h2>
+      <Header></Header>
+      <Navbar></Navbar>
+      <h2 className="text-5xl font-poppins font-bold">This is home</h2>
     </div>
   );
 };
